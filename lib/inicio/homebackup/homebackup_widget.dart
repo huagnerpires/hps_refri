78a1cf92e7331cabe723ec7f49c38ac6ef08805b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-05-26 13:06:08 +0000

    Updating to latest FlutterFlow output.

--- lib/inicio/homebackup/homebackup_widget.dart
+++ lib/inicio/homebackup/homebackup_widget.dart
@@ -1613,7 +1613,7 @@ class _HomebackupWidgetState extends State<HomebackupWidget>
                                                     .fromSTEB(
                                                         20.0, 0.0, 0.0, 0.0),
                                                 child: Text(
-                                                  'Soluções em climatização......KJUYTREWRTYUIOL',
+                                                  'Soluções em climatização....',
                                                   style: FlutterFlowTheme.of(
                                                           context)
                                                       .displaySmall
